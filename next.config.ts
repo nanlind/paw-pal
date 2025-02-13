15c7564fca07a80d496f596e9cbbcc1a41877582
Nan <88483806+nanlind@users.noreply.github.com>
2025-02-13 14:29:19 +0100

    build: add chakra-ui

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["violet-obvious-lizard-845.mypinata.cloud"],
   },
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
